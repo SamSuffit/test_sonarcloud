461052cfd049c34b019f997045a47f895dc8f889
SamSuffit <SamSuffit@users.noreply.github.com>
2024-05-07 13:49:47 +0200

    Create TestService.java

---
+++ src/main/java/com/example/demo/TestService.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestService {
+
+	public  int test() {
+	  return 2;
+	}
+
+}
